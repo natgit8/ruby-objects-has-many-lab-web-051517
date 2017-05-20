74db1829232219671aaf4e90a204fb732ba06d13
Natasha Gitlin <natasha.gitlin8@gmail.com>
2017-05-20 16:14:47 -0400

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,35 @@
+require 'pry'
+
+class Artist
+  attr_accessor :name
+
+  @@song_count = 0
+
+  def initialize(name)
+    @name = name
+    @songs = []
+
+  end
+
+  def add_song(song)
+    self.songs << song
+    song.artist = self
+    @@song_count += 1
+  end
+
+  def songs
+    @songs
+  end
+
+  def add_song_by_name(song)
+    song = Song.new(song)
+    self.songs << song
+    song.artist = self
+    @@song_count += 1
+  end
+
+  def self.song_count
+    @@song_count
+  end
+
+end
